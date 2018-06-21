f344cad227a1e7ca280dd9662db303db1031d3d4
Markus Matiaschek <mmatiaschek@ai-ag.de>
2018-06-21 13:52:31 +0200

    build process for appcenter
    Signed-off-by: Markus Matiaschek <mmatiaschek@ai-ag.de>

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "Building"
+echo " - PROJECT_NUMBER: $PROJECT_NUMBER"
+
+# Updating google-services.json
+
+gsFile=$BUILD_REPOSITORY_LOCALPATH/app/src/release/google-services.json
+
+sed -i '' "s/PROJECT_NUMBER/$PROJECT_NUMBER/g" $gsFile
+sed -i '' "s/PROJECT_ID/$PROJECT_ID/g" $gsFile
+sed -i '' "s/MOBILESDK_APP_ID/$MOBILESDK_APP_ID/g" $gsFile
+sed -i '' "s/API_KEY/$API_KEY/g" $gsFile
+
+# Print out file for reference
+cat $gsFile
+
+echo "Updated id!"
