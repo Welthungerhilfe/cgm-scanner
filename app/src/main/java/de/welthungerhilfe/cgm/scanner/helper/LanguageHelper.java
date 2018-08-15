82ace9af22eb82217964f7ab63654241d64b7507
ZhangNemo34 <zhangnemo34@hotmail.com>
2018-08-15 06:03:58 +0200

    #90 Language is switching to german

---
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/helper/LanguageHelper.java
@@ -0,0 +1,71 @@
+package de.welthungerhilfe.cgm.scanner.helper;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.LocaleList;
+
+import java.util.Locale;
+
+public class LanguageHelper {
+    private static final String SELECTED_LANGUAGE = "Language.Helper.Selected.Language";
+
+    public static Context onAttach(Context context) {
+        String lang = getPersistedData(context);
+        return setLanguage(context, lang);
+    }
+
+    public static String getLanguage(Context context) {
+        return getPersistedData(context);
+    }
+
+    public static Context setLanguage(Context context, String language) {
+        persist(context, language);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResources(context, language);
+        }
+
+        return updateResourcesLegacy(context, language);
+    }
+
+    public static String getPersistedData(Context context) {
+        return new SessionManager(context).getLanguage();
+    }
+
+    private static void persist(Context context, String language) {
+        new SessionManager(context).setLanguage(language);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private static Context updateResources(Context context, String language) {
+
+        Locale locale = new Locale(language);
+
+        Configuration configuration = context.getResources().getConfiguration();
+
+        LocaleList localeList = new LocaleList(locale);
+        localeList.setDefault(localeList);
+        configuration.setLocales(localeList);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+    @SuppressWarnings("deprecation")
+    private static Context updateResourcesLegacy(Context context, String language) {
+
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Resources resources = context.getResources();
+
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+        return context;
+    }
+}
