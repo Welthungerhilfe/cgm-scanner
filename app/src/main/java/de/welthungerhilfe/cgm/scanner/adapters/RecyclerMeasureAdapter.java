b9ceee404e98b39a7918ab2ea9cdce314dfe494a
ZhangNemo34 <zhangnemo34@hotmail.com>
2018-08-29 20:42:16 +0200

    rebasing 1.4.2_SNAPSHOT

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/adapters/RecyclerMeasureAdapter.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/adapters/RecyclerMeasureAdapter.java
@@ -112,14 +112,6 @@ public class RecyclerMeasureAdapter extends RecyclerView.Adapter<RecyclerMeasure
         if (listener != null) {
             holder.bindSelectListener(measureList.get(position));
         }
-
-        if (measure.getType().equals(AppConstants.VAL_MEASURE_MANUAL) && !AppController.getInstance().firebaseConfig.getBoolean(AppConstants.CONFIG_MANUAL_MEASURE_VISIBILITY)) {
-            holder.itemView.setVisibility(View.GONE);
-            holder.itemView.setLayoutParams(new RecyclerView.LayoutParams(0, 0));
-        } else {
-            holder.itemView.setVisibility(View.VISIBLE);
-            setAnimation(holder.itemView, position);
-        }
     }
 
     @Override
