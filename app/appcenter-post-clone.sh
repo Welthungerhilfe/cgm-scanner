1b4046915d157c4c7ac3f4e2e6fc44b2294cb474
Markus Matiaschek <mmatiaschek@gmail.com>
2018-10-16 17:41:01 +0200

    removed non-working playstore deployment code

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -16,14 +16,3 @@ sed -i '' "s/API_KEY/$API_KEY/g" $gsFile
 cat $gsFile
 
 echo "Updated id!"
-
-# set environment for multiple playstore deployments
-echo "Changes for demo playstore deployment"
-if [ -n "$DEPLOYMENT" ]; then
-  echo "setting deployment to $DEPLOYMENT"
-  find . -type f -exec sed -i '' "s/de.welthungerhilfe.cgm.scanner/de.welthungerhilfe.cgm.$DEPLOYMENT/g" {} \;
-  find . -type f -exec sed -i '' "s/cgm\/scanner/cgm\/$DEPLOYMENT/g" {} \;
-  find . -name scanner -type d -exec mv {} {}/../$DEPLOYMENT \;
-else 
-  echo "building for production deployment"
-fi
