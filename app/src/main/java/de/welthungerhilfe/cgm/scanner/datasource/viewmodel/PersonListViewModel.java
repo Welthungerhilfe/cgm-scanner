55f13dedf7d862d4a8b52b24cc93fceb58770e88
ZhangNemo34 <zhangnemo34@hotmail.com>
2019-02-23 08:35:50 -0800

    Undergoing ...

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/datasource/viewmodel/PersonListViewModel.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/datasource/viewmodel/PersonListViewModel.java
@@ -9,19 +9,30 @@ import android.arch.paging.LivePagedListBuilder;
 import android.arch.paging.PagedList;
 import android.support.annotation.NonNull;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import de.welthungerhilfe.cgm.scanner.AppController;
 import de.welthungerhilfe.cgm.scanner.datasource.datasource.person.PersonDataSource;
 import de.welthungerhilfe.cgm.scanner.datasource.models.Person;
 import de.welthungerhilfe.cgm.scanner.datasource.repository.PersonRepository;
+import de.welthungerhilfe.cgm.scanner.utils.PersonFilter;
 
 public class PersonListViewModel extends AndroidViewModel {
     private PersonRepository repository;
 
     private MutableLiveData<List<Person>> personList;
     private MediatorLiveData<List<Person>> liveDataMerger;
+
+    private LiveData<List<Person>> liveData;
+
+    MutableLiveData<PersonFilter> filterLiveData;
+    private PersonFilter filter;
+    private int page;
 
     public PersonListViewModel(@NonNull Application application) {
         super(application);
@@ -30,6 +41,14 @@ public class PersonListViewModel extends AndroidViewModel {
 
         personList = new MutableLiveData<>();
         liveDataMerger = new MediatorLiveData<>();
+
+        filter = new PersonFilter();
+        filterLiveData = new MutableLiveData<>();
+        filterLiveData.setValue(filter);
+        filterLiveData.observe(getApplication(), filter->{
+            getAvailablePersons();
+        });
+        page = 0;
     }
 
     public LiveData<List<Person>> getAll() {
@@ -51,6 +70,14 @@ public class PersonListViewModel extends AndroidViewModel {
         return liveDataMerger;
         */
         return repository.getPersonByPage(page);
+    }
+
+    public PersonFilter getPersonFilter() {
+        return filter;
+    }
+
+    public LiveData<List<Person>> getAvailablePersons() {
+        return repository.getAvailablePersons(filter, page);
     }
 
     public MutableLiveData<List<Person>> getPersonList() {
