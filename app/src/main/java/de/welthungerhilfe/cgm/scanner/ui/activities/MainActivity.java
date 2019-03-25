dc40849d02e66353635ed24aee15946da8a20179
ZhangNemo34 <zhangnemo34@hotmail.com>
2019-03-24 20:19:18 -0700

    Remove unused classes

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/MainActivity.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/MainActivity.java
@@ -255,9 +255,6 @@ public class MainActivity extends BaseActivity implements RecyclerPersonAdapter.
                     case R.id.menuSettings:
                         startActivity(new Intent(MainActivity.this, SettingsActivity.class));
                         break;
-                    case R.id.menuFastUpload:
-                        startActivity(new Intent(MainActivity.this, FastUploadActivity.class));
-                        break;
                     case R.id.menuLogout:
                         AppController.getInstance().firebaseAuth.signOut();
                         session.setSigned(false);
