93fd633afec26aa8a8ffa535177a47eaaaf69d0f
ZhangNemo34 <zhangnemo34@hotmail.com>
2019-07-20 13:34:18 -0700

    addes AZURE_ACCOUNT_NAME to Crashlytics

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/SplashActivity.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/SplashActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.AbstractCrashesListener;
 import com.microsoft.appcenter.crashes.Crashes;
 import com.microsoft.appcenter.crashes.CrashesListener;
 import com.microsoft.appcenter.crashes.ingestion.models.ErrorAttachmentLog;
 import com.microsoft.appcenter.crashes.model.ErrorReport;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import de.welthungerhilfe.cgm.scanner.R;
+import de.welthungerhilfe.cgm.scanner.helper.AppConstants;
 
 public class SplashActivity extends AppCompatActivity {
     public void onCreate(Bundle saveBundle) {
@@ -20,6 +25,16 @@ public class SplashActivity extends AppCompatActivity {
         boolean isEnabled = Crashes.isEnabled().get();
         if (!isEnabled)
             Crashes.setEnabled(true);
+
+        AbstractCrashesListener customListener = new AbstractCrashesListener() {
+            @Override
+            public Iterable<ErrorAttachmentLog> getErrorAttachments(ErrorReport report) {
+                ErrorAttachmentLog azureAccount = ErrorAttachmentLog.attachmentWithText("Azure Account Name : " + AppConstants.AZURE_ACCOUNT_NAME, "azure_account.txt");
+
+                return Collections.singletonList(azureAccount);
+            }
+        };
+        Crashes.setListener(customListener);
 
         super.onCreate(saveBundle);
         setContentView(R.layout.activity_splash);
