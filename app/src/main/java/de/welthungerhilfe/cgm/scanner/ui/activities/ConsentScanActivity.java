cb28c8446afe1fa87a5458754025c7dd4b8e2171
ZhangNemo34 <zhangnemo34@hotmail.com>
2019-08-05 01:03:18 -0700

    adds camera 2 api

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/ConsentScanActivity.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/ConsentScanActivity.java
@@ -1,45 +1,65 @@
 package de.welthungerhilfe.cgm.scanner.ui.activities;
 
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.welthungerhilfe.cgm.scanner.R;
 
-public class ConsentScanActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+public class ConsentScanActivity extends AppCompatActivity {
 
     @BindView(R.id.cameraPreview)
-    SurfaceView cameraPreview;
-    private SurfaceHolder cameraHolder;
+    TextureView cameraPreview;
+
+
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder previewBuilder;
+    private CameraCaptureSession previewSession;
+
+    private static final SparseIntArray ORIENTATIONS=new SparseIntArray();
+    static
+    {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
+
+
 
     protected void onCreate(Bundle savedBundle) {
         super.onCreate(savedBundle);
         setContentView(R.layout.activity_consent_scan);
         ButterKnife.bind(this);
-
-        initCamera();
     }
 
-    private void initCamera() {
-        cameraHolder = cameraPreview.getHolder();
-        cameraHolder.addCallback(this);
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
+    private void takePicture()
+    {
 
     }
 }
