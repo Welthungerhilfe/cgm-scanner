a1551b1fed8a893241e368862fcd7132b7c5359c
prajurock <prajusingh78655@gmail.com>
2019-10-16 14:45:13 +0530

    Function getScanQuality Added

---
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/datasource/dao/ArtifactResultDao.java
@@ -0,0 +1,24 @@
+package de.welthungerhilfe.cgm.scanner.datasource.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import de.welthungerhilfe.cgm.scanner.datasource.models.ArtifactResult;
+
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
+import static de.welthungerhilfe.cgm.scanner.datasource.database.CgmDatabase.TABLE_ARTIFACT_RESULT;
+
+@Dao
+public interface ArtifactResultDao {
+    @Insert(onConflict = REPLACE)
+    void insertArtifact_quality(ArtifactResult artifactResuslt);
+
+    @Query("SELECT real FROM " + TABLE_ARTIFACT_RESULT )
+    List<Double> getArtifactResult();
+}
+
+
