6427d8fb9273a378c9bf92b0087ff3197a8e6828
Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>
2020-12-10 11:36:34 +0100

    another attempt to get error body during upload file

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/network/service/UploadService.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/network/service/UploadService.java
@@ -317,7 +317,9 @@ public class UploadService extends Service implements OnFileLogsLoad {
                     Log.i(TAG, "this is response onError uploadfiles " + e.getMessage() + file.getPath());
                     AuthenticationHandler authentication = AuthenticationHandler.getInstance();
                     if (authentication.isExpiredToken(e.getMessage())) {
-                        Log.d(TAG, "error body=" + this.toString());
+                        Log.d(TAG, "error single=" + request.blockingSingle());
+                        Log.d(TAG, "error first=" + request.blockingFirst());
+                        Log.d(TAG, "error last=" + request.blockingLast());
                         authentication.updateToken((email, token, feedback) -> updateFileLog(log));
                     } else {
                         updateFileLog(log);
