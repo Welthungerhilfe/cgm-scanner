80ffb4ea1590f6636d9a64ab8c0b57e2521edb8f
Luboš Vonásek <tridosm@gmail.com>
2021-06-25 11:13:02 +0200

    tango camera implementation revision (#368)
    Co-authored-by: Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/hardware/camera/TangoCamera.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/hardware/camera/TangoCamera.java
@@ -21,10 +21,8 @@ package de.welthungerhilfe.cgm.scanner.hardware.camera;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.opengl.GLSurfaceView;
-import android.util.Log;
-import android.view.Display;
-import android.view.Surface;
 import android.widget.ImageView;
 
 import com.google.atap.tangoservice.Tango;
@@ -41,12 +39,14 @@ import com.microsoft.appcenter.crashes.Crashes;
 import com.projecttango.tangosupport.TangoSupport;
 
 import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import de.welthungerhilfe.cgm.scanner.hardware.gpu.BitmapHelper;
 import de.welthungerhilfe.cgm.scanner.utils.Utils;
 
-public class TangoCamera extends AbstractARCamera {
+public class TangoCamera extends AbstractARCamera implements GLSurfaceView.Renderer {
 
     public interface TangoCameraListener {
         void onTangoColorData(TangoImageBuffer tangoImageBuffer);
@@ -55,26 +55,14 @@ public class TangoCamera extends AbstractARCamera {
     }
 
     //constants
-    private static final String TAG = TangoCamera.class.getSimpleName();
     private static final String TANGO_CORE = "com.google.tango";
-    private static final int INVALID_TEXTURE_ID = 0;
 
     //Tango objects and status
     private Tango mTango;
-    private TangoConfig mConfig;
-    private boolean mIsConnected;
     private boolean mPointCloudAvailable;
-    private int mConnectedTextureIdGlThread = INVALID_TEXTURE_ID;
-    private int mDisplayRotation = Surface.ROTATION_0;
-    private Semaphore mutex_on_mIsRecording;
-    private AtomicBoolean mIsFrameAvailableTangoThread;
-    private TangoCameraRenderer mRenderer;
 
     public TangoCamera(Activity activity) {
         super(activity, DepthPreviewMode.OFF, PreviewSize.CLIPPED);
-
-        mIsConnected = false;
-        mIsFrameAvailableTangoThread = new AtomicBoolean(false);
     }
 
     @Override
@@ -82,48 +70,7 @@ public class TangoCamera extends AbstractARCamera {
         super.onCreate(colorPreview, depthPreview, surfaceview);
 
         mGLSurfaceView.setEGLContextClientVersion(2);
-        mRenderer = new TangoCameraRenderer(new TangoCameraRenderer.RenderCallback() {
-
-            @Override
-            public void preRender() {
-
-                // This is the work that you would do on your main OpenGL render thread.
-
-                // We need to be careful to not run any Tango-dependent code in the OpenGL
-                // thread unless we know the Tango Service to be properly set up and connected.
-                if (!mIsConnected) {
-                    return;
-                }
-
-                try {
-                    // Synchronize against concurrently disconnecting the service triggered from the
-                    // UI thread.
-                    synchronized (this) {
-                        // Connect the Tango SDK to the OpenGL texture ID where we are going to
-                        // render the camera.
-                        // NOTE: This must be done after the texture is generated and the Tango
-                        // service is connected.
-                        if (mConnectedTextureIdGlThread == INVALID_TEXTURE_ID) {
-                            mConnectedTextureIdGlThread = mRenderer.getTextureId();
-                            mTango.connectTextureId(TangoCameraIntrinsics.TANGO_CAMERA_COLOR, mConnectedTextureIdGlThread);
-
-                            Log.d(TAG, "connected to texture id: " + mRenderer.getTextureId());
-                        }
-
-                        // If there is a new RGB camera frame available, update the texture and
-                        // scene camera pose.
-                        if (mIsFrameAvailableTangoThread.compareAndSet(true, false)) {
-
-                            mTango.updateTexture(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
-                        }
-                    }
-                } catch (TangoErrorException e) {
-                    Crashes.trackError(e);
-                }
-            }
-        });
-
-        mGLSurfaceView.setRenderer(mRenderer);
+        mGLSurfaceView.setRenderer(this);
     }
 
     @Override
@@ -154,12 +101,8 @@ public class TangoCamera extends AbstractARCamera {
         synchronized (this) {
             try {
                 mTango.disconnectCamera(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
-                // We need to invalidate the connected texture ID so that we cause a
-                // re-connection in the OpenGL thread after resume.
-                mConnectedTextureIdGlThread = INVALID_TEXTURE_ID;
                 mTango.disconnect();
                 mTango = null;
-                mIsConnected = false;
             } catch (TangoErrorException e) {
                 Crashes.trackError(e);
             }
@@ -182,20 +125,20 @@ public class TangoCamera extends AbstractARCamera {
         }
 
         mPointCloudAvailable = false;
-        mutex_on_mIsRecording = new Semaphore(1,true);
 
         mTango = new Tango(mActivity, () -> {
             // Synchronize against disconnecting while the service is being used in
             // the OpenGL thread or in the UI thread.
             synchronized (this) {
                 try {
-                    mConfig = setupTangoConfig(mTango);
-                    mTango.connect(mConfig);
+                    TangoConfig config = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+                    config.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
+                    config.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+                    config.putInt(TangoConfig.KEY_INT_DEPTH_MODE, TangoConfig.TANGO_DEPTH_MODE_POINT_CLOUD);
+
+                    mTango.connect(config);
                     startupTango();
                     TangoSupport.initialize(mTango);
-                    mIsConnected = true;
-
-                    setDisplayRotation();
                 } catch (TangoOutOfDateException e) {
                     mActivity.runOnUiThread(() -> {
                         Utils.openPlayStore(mActivity, TANGO_CORE);
@@ -210,20 +153,15 @@ public class TangoCamera extends AbstractARCamera {
         });
     }
 
-    private TangoConfig setupTangoConfig(Tango tango) {
-        // Create a new Tango configuration and enable the Camera API.
-        TangoConfig config = tango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
-        config.putBoolean(TangoConfig.KEY_BOOLEAN_COLORCAMERA, true);
-        config.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
-        config.putInt(TangoConfig.KEY_INT_DEPTH_MODE, TangoConfig.TANGO_DEPTH_MODE_POINT_CLOUD);
-        return config;
-    }
-
     private void startupTango() {
         // Lock configuration and connect to Tango.
         // Select coordinate frame pair.
         final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<>();
         framePairs.add(new TangoCoordinateFramePair(TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE, TangoPoseData.COORDINATE_FRAME_DEVICE));
+        TangoCameraIntrinsics[] calibration = {
+                mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_COLOR),
+                mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_DEPTH)
+        };
 
         // Listen for new Tango data.
         mTango.connectListener(framePairs, new Tango.TangoUpdateCallback() {
@@ -231,12 +169,6 @@ public class TangoCamera extends AbstractARCamera {
             @Override
             public void onPoseAvailable(TangoPoseData pose) {
                 if (pose.statusCode == TangoPoseData.POSE_VALID) {
-                    try {
-                        mutex_on_mIsRecording.acquire();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        Crashes.trackError(e);
-                    }
                     pose = TangoSupport.getPoseAtTime(0,
                             TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
                             TangoPoseData.COORDINATE_FRAME_CAMERA_DEPTH,
@@ -250,24 +182,12 @@ public class TangoCamera extends AbstractARCamera {
                     mRotation[1] = (float) pose.rotation[1];
                     mRotation[2] = (float) pose.rotation[2];
                     mRotation[3] = (float) pose.rotation[3];
-                    mutex_on_mIsRecording.release();
                 }
             }
 
             @Override
             public void onPointCloudAvailable(TangoPointCloudData pointCloudData) throws TangoErrorException {
                 mPointCloudAvailable = true;
-
-                try {
-                    mutex_on_mIsRecording.acquire();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Crashes.trackError(e);
-                }
-                TangoCameraIntrinsics[] calibration = {
-                        mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_COLOR),
-                        mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_DEPTH)
-                };
                 mColorCameraIntrinsic[0] = (float)calibration[0].fx / (float)calibration[0].width;
                 mColorCameraIntrinsic[1] = (float)calibration[0].fy / (float)calibration[0].height;
                 mColorCameraIntrinsic[2] = (float)calibration[0].cx / (float)calibration[0].width;
@@ -286,41 +206,6 @@ public class TangoCamera extends AbstractARCamera {
                 }
                 for (Object listener : mListeners) {
                     ((TangoCameraListener)listener).onTangoDepthData(pointCloudData, position, rotation, calibration);
-                }
-                mutex_on_mIsRecording.release();
-            }
-
-
-            @Override
-            public void onFrameAvailable(int cameraId) {
-                // This will get called every time a new RGB camera frame is available to be
-                // rendered.
-                //Log.d(TAG, "onFrameAvailable");
-
-                if (cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
-                    // Now that we are receiving onFrameAvailable callbacks, we can switch
-                    // to RENDERMODE_WHEN_DIRTY to drive the render loop from this callback.
-                    // This will result in a frame rate of approximately 30FPS, in synchrony with
-                    // the RGB camera driver.
-                    // If you need to render at a higher rate (i.e., if you want to render complex
-                    // animations smoothly) you  can use RENDERMODE_CONTINUOUSLY throughout the
-                    // application lifecycle.
-                    if (mGLSurfaceView.getRenderMode() != GLSurfaceView.RENDERMODE_WHEN_DIRTY) {
-                        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-                    }
-
-                    // Note that the RGB data is not passed as a parameter here.
-                    // Instead, this callback indicates that you can call
-                    // the {@code updateTexture()} method to have the
-                    // RGB data copied directly to the OpenGL texture at the native layer.
-                    // Since that call needs to be done from the OpenGL thread, what we do here is
-                    // set up a flag to tell the OpenGL thread to do that in the next run.
-                    // NOTE: Even if we are using a render-by-request method, this flag is still
-                    // necessary since the OpenGL thread run requested below is not guaranteed
-                    // to run in synchrony with this requesting call.
-                    mIsFrameAvailableTangoThread.set(true);
-                    // Trigger an OpenGL render to update the OpenGL scene with the new RGB data.
-                    mGLSurfaceView.requestRender();
                 }
             }
 
@@ -345,23 +230,33 @@ public class TangoCamera extends AbstractARCamera {
             if ( ! mPointCloudAvailable) {
                 return;
             }
-            mPixelIntensity = TangoUtils.getPixelIntensity(tangoImageBuffer);
+
+            //update preview window
+            Bitmap bitmap = TangoUtils.imageBuffer2Bitmap(tangoImageBuffer);
+            mActivity.runOnUiThread(() -> {
+                float scale = getPreviewScale(bitmap);
+                mColorCameraPreview.setImageBitmap(bitmap);
+                mColorCameraPreview.setRotation(90);
+                mColorCameraPreview.setScaleX(scale);
+                mColorCameraPreview.setScaleY(scale);
+                mDepthCameraPreview.setRotation(90);
+                mDepthCameraPreview.setScaleX(scale);
+                mDepthCameraPreview.setScaleY(scale);
+            });
+
+            mPixelIntensity = BitmapHelper.getPixelIntensity(bitmap);
             for (Object listener : mListeners) {
                 ((TangoCameraListener)listener).onTangoColorData(tangoImageBuffer);
             }
         });
     }
 
-    private void setDisplayRotation() {
-        Display display = mActivity.getWindowManager().getDefaultDisplay();
-        mDisplayRotation = display.getRotation();
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {}
 
-        // We also need to update the camera texture UV coordinates. This must be run in the OpenGL
-        // thread.
-        mGLSurfaceView.queueEvent(() -> {
-            if (mIsConnected) {
-                mRenderer.updateColorCameraTextureUv(mDisplayRotation);
-            }
-        });
-    }
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {}
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {}
 }
