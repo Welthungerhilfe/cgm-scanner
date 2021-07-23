2fcc1ce20340d5abe8c53144bca3086ef5aafa9b
Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>
2021-07-23 08:13:25 +0200

    Wrong unit test removed

--- app/src/test/java/de/welthungerhilfe/cgm/scanner/utils/UtilsTest.java
+++ app/src/test/java/de/welthungerhilfe/cgm/scanner/utils/UtilsTest.java
@@ -137,10 +137,4 @@ public class UtilsTest {
         decimalNumber = Utils.checkDoubleDecimals(str);
         assertThat(1, is(decimalNumber));
     }
-
-    @Test
-    public void validateStdTestQrCodeTest() {
-        boolean isValid = Utils.isValidateStdTestQrCode("STD_TEST_20210721");
-        assertThat(true, isValid);
-    }
 }
