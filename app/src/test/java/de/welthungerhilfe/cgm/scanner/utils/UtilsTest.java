3ea00f6abcd3d4919669f15df5faad264d24c65c
Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>
2021-07-23 07:53:16 +0200

    remove unit test for removed method

--- app/src/test/java/de/welthungerhilfe/cgm/scanner/utils/UtilsTest.java
+++ app/src/test/java/de/welthungerhilfe/cgm/scanner/utils/UtilsTest.java
@@ -139,12 +139,6 @@ public class UtilsTest {
     }
 
     @Test
-    public void testStdQrCodedateExtractor() {
-        String str = Utils.extractDatesFromStdTestQrCode("STD_TEST_20210721");
-        assertThat("20210721", is(str));
-    }
-
-    @Test
     public void validateStdTestQrCodeTest() {
         boolean isValid = Utils.isValidateStdTestQrCode("STD_TEST_20210721");
         assertThat(true, is(isValid));
