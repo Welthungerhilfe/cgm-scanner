0093208eafff0f65222f14620f450de40b1053dc
Luboš Vonásek <tridosm@gmail.com>
2022-01-11 14:00:42 +0000

    Scan feedback based on AREngine pose detection (#397)
    * Show skeleton feedback
    
    * Improve visualisation relability
    
    * Render outline instead of skeleton
    
    * Better bones validation
    
    * AR mode for device check fixed
    
    * Scan feedback reworked
    
    * Code esthetical adjustment
    
    * Static outline fixed
    
    Co-authored-by: Lubos Vonasek <Lubos.Vonasek@welthungerhilfe.de>

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/ScanModeActivity.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/ui/activities/ScanModeActivity.java
@@ -246,6 +246,7 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
     private final Object threadsLock = new Object();
 
     private AbstractARCamera mCameraInstance;
+    private ImageView mOutline;
 
     public void onStart() {
         super.onStart();
@@ -314,6 +315,7 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
 
         findViewById(R.id.imgClose).setOnClickListener(this);
 
+        mOutline = findViewById(R.id.scanOutline);
         ImageView colorCameraPreview = findViewById(R.id.colorCameraPreview);
         ImageView depthCameraPreview = findViewById(R.id.depthCameraPreview);
         GLSurfaceView glSurfaceView = findViewById(R.id.surfaceview);
@@ -487,7 +489,6 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
     }
 
     private void openScan() {
-        getCamera().resetTrackingState();
         fab.setImageResource(R.drawable.recorder);
         activityScanModeBinding.lytScanner.setVisibility(View.VISIBLE);
         mTxtFeedback.setVisibility(View.GONE);
@@ -740,22 +741,19 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
 
     private void onFeedbackUpdate() {
         AbstractARCamera.LightConditions light = getCamera().getLightConditionState();
-        AbstractARCamera.TrackingState tracking = getCamera().getTrackingState();
-        float distance = getCamera().getTargetDistance();
+        boolean childDetected = getCamera().getPersonCount() == 1;
         runOnUiThread(() -> {
 
-            if (SCAN_MODE == AppConstants.SCAN_STANDING) {
-                switch (tracking) {
-                    case INIT:
-                    case TRACKED:
-                        setFeedback(null);
-                        break;
-                    case LOST:
-                        setFeedback(getString(R.string.score_not_detected));
-                        break;
-                }
-            } else {
+            if ((SCAN_MODE == AppConstants.SCAN_LYING) && (SCAN_STEP != AppConstants.SCAN_LYING_FRONT)) {
+                mOutline.setVisibility(View.GONE);
+                getCamera().setSkeletonMode(AbstractARCamera.SkeletonMode.OFF);
+            } else if (childDetected) {
+                mOutline.setVisibility(View.GONE);
+                getCamera().setSkeletonMode(AbstractARCamera.SkeletonMode.OUTLINE);
                 setFeedback(null);
+            } else {
+                mOutline.setVisibility(View.VISIBLE);
+                getCamera().setSkeletonMode(AbstractARCamera.SkeletonMode.OFF);
             }
 
             if (mTxtFeedback.getVisibility() == View.GONE) {
@@ -769,16 +767,6 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
                     case DARK:
                         setFeedback(getString(R.string.score_light_dark));
                         break;
-                }
-            }
-
-            if ((mTxtFeedback.getVisibility() == View.GONE) && (distance != 0)) {
-                if (distance < 0.7) {
-                    setFeedback(getString(R.string.score_distance_close));
-                } else if (distance > 1.5f) {
-                    setFeedback(getString(R.string.score_distance_far));
-                } else {
-                    setFeedback(null);
                 }
             }
         });
@@ -834,12 +822,8 @@ public class ScanModeActivity extends BaseActivity implements View.OnClickListen
                     break;
             }
 
-            //set information if child is detected (note: this is unsupported on ARCore devices and for lying children)
-            boolean childDetected = false;
-            if (SCAN_MODE == AppConstants.SCAN_STANDING) {
-                AbstractARCamera.TrackingState tracking = getCamera().getTrackingState();
-                childDetected = tracking == AbstractARCamera.TrackingState.TRACKED;
-            }
+            //set information if child is detected (note: this is unsupported on ARCore devices and for lying children wrongly oriented)
+            boolean childDetected = getCamera().getPersonCount() == 1;
             log.setChildDetected(childDetected);
             log.setChildHeight(childHeight);
 
