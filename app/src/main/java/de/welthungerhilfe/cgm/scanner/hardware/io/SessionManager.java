280e68d7b1ed3f2c3602aaa1e7b6764399ca4f1b
jay desai <jaydesai111@yahoo.com>
2026-01-06 16:53:56 +0530

    releasing final apk to internal testing track

--- app/src/main/java/de/welthungerhilfe/cgm/scanner/hardware/io/SessionManager.java
+++ app/src/main/java/de/welthungerhilfe/cgm/scanner/hardware/io/SessionManager.java
@@ -429,7 +429,7 @@ public class SessionManager {
     }
 
     public String getAppSecret() {
-        return pref.getString(APP_SECRET, "TEST");
+        return pref.getString(APP_SECRET, null);
     }
 
     public void setAppSecret(String secret) {
